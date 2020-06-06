ec8bde19168c98ea56b80b0c6db3688322a77c21
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-06-06 09:43:21 +0300

    defined my URLconf for our awards app we need to connect it to our project.

--- awards/urls.py
+++ awards/urls.py
@@ -13,9 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from awewards import views
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+
+app_name='ratings'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('awewards.urls')),
+    url(r'^accounts/logout/', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    # url(r'^logout/$', views.logout, name='logout'), # To be implemented later 
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
+    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^search/$', views.search, name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
